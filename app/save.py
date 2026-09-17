36a5e7d0ea39dc0b12414e29a7e67d81aaf7f04b
Iniubong Arthur <arthurshur@gmail.com>
2026-09-17 23:08:32 +0000

    the revamp begins

--- app/save.py
+++
@@ -1,8 +0,0 @@
-from app import db
-from john import models
-
-
-user1 = User("Dula","dimka@gmail.com","1234")
-
-db.session.add(user)
-db.session.commit()
--- save.py
+++
@@ -1,9 +0,0 @@
-from app import db, create_app
-from app import models
-
-app = create_app()
-with app.app_context():
-    user = models.User(username = "admin",email = "arthurshur@gmail.com")
-    user.set_password("1234")
-    db.session.add(user)
-    db.session.commit()
