59f0b3b4109570f4995ed357b5442aa3737b7054
Iniubong Arthur <arthurshur@gmail.com>
2026-09-17 23:17:12 +0000

    the revamp begins

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-from app import create_app
-
-app = create_app()
-
-if __name__ =='__main__':
-    port = int(os.environ.get('PORT', 8000))  # Use the PORT environment variable or default to 8000
-    app.run(port=port, debug = True)
