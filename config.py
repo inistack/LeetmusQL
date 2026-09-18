37c5cdaa8d1825b5c988b4f99bf51df189800431
Iniubong Arthur <arthurshur@gmail.com>
2026-09-18 23:28:45 +0000

    chore: config.py setup

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    if not SECRET_KEY:
+        raise ValueError("SECRET_KEY not set in .env file")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABSE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RESEND_API_KEY = os.environ.get("RESEND_API_KEY")
+    CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
+    CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
